76fa51c1fe97def6a85dc8f526377b35ee4b390a
Christian König <koenig.kersting@gmail.com>
2023-11-15 21:30:10 +0100

    added: deployment files for heroku

---
+++ init.R
@@ -0,0 +1,10 @@
+# tell the buildpack to install the required packages:
+# using the helper caches the installed packages between deployments afaik.
+helpers.installPackages(
+  "exams",
+  "xtable",
+  "tth"
+)
+
+# special case of installing iuftools from source
+install.packages("./iuftools", repos = NULL, type="source")
