2fe3b3fd38afbc6b93127828e3497cd203efe418
guesswho1234 <sbachler@gmx.at>
2024-01-28 20:40:35 +0100

    test: install newest exams version from rforge

--- init.R
+++ init.R
@@ -3,7 +3,7 @@ helpers.installPackages(
   "shinyjs",
   "shinyWidgets",
   "shinycssloaders",
-  "exams",
+  # "exams",
   "xtable",
   "tth",
   "png",
@@ -17,7 +17,7 @@ helpers.installPackages(
 
 # special install packages
 install.packages("./iuftools", repos=NULL, type="source")
-# install.packages("exams", repos="http://R-Forge.R-project.org") # need new version for handling faulty scans properly
+install.packages("exams", repos="http://R-Forge.R-project.org") # need new version for handling faulty scans properly
 
 # tex setup
 tinytex::install_tinytex()
