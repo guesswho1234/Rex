258a67d0649e42d58cfa0444b1d2988c83b41aca
guesswho1234 <sebastian.bachler@uibk.ac.at>
2024-05-16 18:58:12 +0200

    feat: inject addon button to exercise info

--- www/addons/llmAssistant/llmAssistant_script.js
+++ www/addons/llmAssistant/llmAssistant_script.js
@@ -1,1 +1,3 @@
 let llmAssistant = new Object();
+
+$("#exercise_info").prepend('<div id="exerciseLlmAssistant"><div class="exerciseLlmAssistantButton"><span class="iconButton"><i class="fa-solid fa-wand-magic-sparkles"></i></span><span class="textButton"><span lang="de">KI Assistent</span><span style="display: none;" lang="en">AI assistant</span></span></div></div>');
