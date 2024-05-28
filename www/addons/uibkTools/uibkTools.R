18a524e86b9356c151151e294d733eae80cce0f5
guesswho1234 <sebastian.bachler@uibk.ac.at>
2024-05-28 11:07:33 +0200

    fix: change header text in exercises tab

--- www/addons/uibkTools/uibkTools.R
+++ www/addons/uibkTools/uibkTools.R
@@ -112,6 +112,7 @@ if(!any(installed.packages()[,1]=="iuftools"))
     
     evalData = Reduce(rbind, lapply(args$rexEvaluationLists, \(x){
       content = read.table(text=x[[3]], sep=";", header = TRUE, colClasses = "character", fill=TRUE)
+	  print(head(content))
     }))
     
     if(is.null(evalData))
