2097cda8b448f0b97db2a68fb5ad07a726e4f089
guesswho1234 <sebastian.bachler@uibk.ac.at>
2024-06-26 15:46:48 +0200

    fix: memeory overflow on finalize evaluation

--- www/script.js
+++ www/script.js
@@ -3125,12 +3125,9 @@ Shiny.addCustomMessageHandler('evaluationStatistics', function(jsonData) {
 });
 
 $('body').on('click', '#proceedEval', function() {
-	const scans_reg_fullJoinData = rex.examEvaluation.scans_reg_fullJoinData;
 	const properties = ['scan', 'sheet', 'scrambling', 'type', 'replacement', 'registration'].concat(new Array(45).fill(1).map( (_, i) => i+1 ));
-	const datenTxt = Object.assign({}, scans_reg_fullJoinData.filter(x => scanValid(x)).map(x => Object.assign({}, properties.map(y => x[y] === undefined ? "00000" : x[y], {}))));
+	const datenTxt = Object.assign({}, rex.examEvaluation.scans_reg_fullJoinData.filter(x => scanValid(x)).map(x => Object.assign({}, properties.map(y => x[y] === undefined ? "00000" : x[y], {}))));
 	
-	//todo: memory overflow: scans_reg_fullJoinData is the problem
-	// Shiny.onInputChange("proceedEvaluation", {scans_reg_fullJoinData:scans_reg_fullJoinData, datenTxt:datenTxt}, {priority: 'event'});
 	Shiny.onInputChange("proceedEvaluation", {scans_reg_fullJoinData:null, datenTxt:datenTxt}, {priority: 'event'});
 });
 
