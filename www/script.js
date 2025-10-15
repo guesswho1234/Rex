6e05e873dc99d6b5f3f1574517bfaf2299f1ba02
guesswho1234 <sbachler@gmx.at>
2025-10-15 15:23:27 +0200

    style: change popout menu style

--- www/script.js
+++ www/script.js
@@ -801,52 +801,6 @@ $('#navPopout').click(function () {
 	}
 });
 
-dragElement(document.getElementById("nav-popout-container"));
-
-function dragElement(element) {
-  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  
-  if (document.getElementById("nav-popout-container")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById("nav-popout-container").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    element.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    $('#nav-popout-container').removeClass("drag");
-	
-    e = e || window.event;
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    $('#nav-popout-container').addClass("drag");
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    element.style.top = (element.offsetTop - pos2) + "px";
-    element.style.left = (element.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-
 /* --------------------------------------------------------------
  BUTTON MODE 
 -------------------------------------------------------------- */
